1bd0f9cadf5c16f6642dcff418681ed3cc668dac
Quentin Kabasele <qkab78@Mac-de-Developer-2.local>
2017-01-08 20:54:38 +0100

    Mise à jour du fichier 'access.php'

--- Annexes/H&T/secure/access.php
+++ Annexes/H&T/secure/access.php
@@ -43,18 +43,35 @@ class access{
 
 
     //Insérer un user
-    public function regiterUser($username, $password, $salt, $email, $firstname, $lastname){
+    public function registerUser($username, $password, $email, $firstname, $lastname){
         //Requête SQL
-        $req = "INSERT INTO users SET username=?, password=?, salt=?, email=?, firstname=?, lastname=?";
+        $req = "INSERT INTO users SET username=?, password=?, email=?, firstname=?, lastname=?";
         $statement = $this->conn->prepare($req);
         //S'il y a une erreur
         if (!$statement){
             throw new Exception($statement->error);
         }
-        //Bind 5 params de type string et on les prépare à les mettredans notre table 'users'
-        $statement->bind_param("ssssss", $username, $password, $salt, $email, $firstname, $lastname);
+        //Bind 5 params de type string et on les prépare à les mettre dans notre table 'users'
+        $statement->bind_param("sssss", $username, $password, $email, $firstname, $lastname);
         $result = $statement->execute();
         return $result;
+    }
+
+    public function loginUser($username, $password){
+        //Requête à exécuter
+        $req = "SELECT * from users WHERE username='".$username."' AND password='".$password."' ";
+        //Execution de a requête grâce à 'query'
+        $result = $this->conn->query($req);
+
+        //Si on a au moins 1 résultat
+        if ($result != null && (mysqli_num_rows($result) >= 1) ){
+            $row = $result->fetch_array(MYSQLI_ASSOC);
+            if (!empty($row)){
+                $returnArray = $row;
+            }
+        }
+
+        return $returnArray;
     }
 
     public function getUserInformations($username){
@@ -74,20 +91,53 @@ class access{
         return $returnArray;
     }
 
-    public function getLoginInformations($username){
-        //Requête à exécuter
-        $query = "SELECT * from users WHERE username='".$username."' ";
-        //Execution de a requête grâce à 'query'
-        $res = $this->conn->query($query);
 
-        if ($res != null && (mysqli_num_rows($res) >= 1) ){
-            $row = $res->fetch_array(MYSQLI_ASSOC);
+    public function insertMessage($username, $message){
+        //Requête à effectuer
+        $req =  "INSERT INTO messages SET username=?, message=?";
+        $statement = $this->conn->prepare($req);
+
+        //S'il y a une erreur
+        if (!$statement){
+            throw new Exception($statement->error);
+        }
+        //Bind 2 params de type string et on les prépare à les mettre dans notre table 'messages'
+        $statement->bind_param("ss", $username, $message);
+        $result = $statement->execute();
+        return $result;
+    }
+
+    public function getAllMessages($username){
+        //Requête à exécuter
+        $req = "SELECT * from messages WHERE username='".$username."' ";
+        //Execution de a requête grâce à 'query'
+        $result = $this->conn->query($req);
+
+        //Si on a au moins 1 résultat
+        if ($result != null && (mysqli_num_rows($result) >= 1) ){
+            $row = $result->fetch_array(MYSQLI_ASSOC);
             if (!empty($row)){
                 $returnArray = $row;
             }
         }
 
         return $returnArray;
+    }
+
+    public function getAllUsers(){
+        $req = "SELECT firstname, lastname, username from users";
+        $result = $this->conn->query($req);
+
+        if ($result != null && (mysqli_num_rows($result) >= 1) ){
+            $returnArray = array();
+            while ($row = $result->fetch_array(MYSQLI_ASSOC)){
+                if (!empty($row)){
+                    $returnArray[] = $row;
+                }
+            }
+                    //echo json_encode($returnArray);
+        }
+        echo '{"users": '.json_encode($returnArray).'}';
     }
 }
 ?>
